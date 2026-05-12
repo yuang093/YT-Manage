ec8b5545f65b2c4ac2fa56f82a4ecb59504c8459
yuang093 <yuang093@gmail.com>
2026-05-12 20:05:47 +0800

    fix: add missing LoginView import

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,7 @@ import Dashboard from './components/Dashboard/Dashboard';
 import CreatePage from './components/Form/CreatePage';
 import EditPage from './components/Form/EditPage';
 import AdminPanel from './components/Admin/AdminPanel';
+import LoginView from './components/Login/LoginView';
 // import PlayerView from './components/Player/PlayerView';
 
 // --- 全局動畫樣式 ---
